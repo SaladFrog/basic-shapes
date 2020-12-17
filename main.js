ade2f5612b3d613c8e2a6cdd80e65d49ba89cf17
SaladFrog <48181364+SaladFrog@users.noreply.github.com>
2020-12-17 10:18:04 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,42 @@
+var canvas = document.getElementById("canvas");
+var context = canvas.getContext("2d");
+
+var count = 30;
+
+// create array with points
+function createPlot() {
+  var points = []
+  for (let i = 0; i < count; i++) {
+    var scale = Math.min(canvas.width, canvas.height) / 2;
+    var interval = Math.PI * 2 / count;
+    var desiredRadianAngleOnCircle = interval * i;
+
+    var position = circle([], scale * 0.5, desiredRadianAngleOnCircle);
+    position[0] += canvas.width / 2;
+    position[1] += canvas.height / 2;
+
+    points.push(position)
+  }
+  return points;
+}
+
+// create point for circle
+function circle(out, scale, dpi) {
+  // used to randomize dots in a circle shape
+  //var r = Math.random() * Math.PI * 2.0;
+  var r = dpi;
+  out[0] = Math.cos(r) * scale;
+  out[1] = Math.sin(r) * scale;
+  return out;
+}
+
+// fetch the created array
+const particles = createPlot();
+
+// draw to canvas
+particles.forEach(p => {
+  context.beginPath();
+  context.arc(p[0], p[1], 3, 0, Math.PI * 2);
+  context.stroke();
+})
+
