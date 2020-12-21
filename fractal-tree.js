6bbaadd8c7768bb10b18885c3a23469e4ed621fa
SaladFrog <48181364+SaladFrog@users.noreply.github.com>
2020-12-21 18:28:50 +0100

    Update fractal-tree.js
    complete

--- fractal-tree.js
+++ fractal-tree.js
@@ -2,28 +2,36 @@
 * Coding Challenge #14: Fractal Trees - Recursive
 * https://www.youtube.com/watch?v=0jjeOYMjmDU
 */
-
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
 var height = 400;
 var width = 400;
+var angle = Math.PI / 4;
 
 function draw() {
   ctx.translate(200, height);
+  ctx.beginPath();
   branch(100);
-}
-
-ctx.beginPath();
-
-function branch(len) { 
-  ctx.lineTo(0,0);
-  ctx.lineTo(0, -len);
-  ctx.translate(0, -len);
-  ctx.rotate(Math.PI / 4);
-  if (len > 4) {
-    branch(len * 0.67);
-  }
   ctx.stroke();
 }
+
+function branch(len) {
+  ctx.moveTo(0, 0);
+  ctx.lineTo(0, -len);
+  ctx.translate(0, -len);
+  if (len > 4) {
+    // to the right to the right
+    ctx.save();
+    ctx.rotate(angle);
+    branch(len * 0.67);
+    ctx.restore();
+    // to the left to the left
+    ctx.save();
+    ctx.rotate(-angle);
+    branch(len * 0.67);
+    ctx.restore();
+  }
+}
 draw();
+
