8edb4494b2fa4395aa4c7b6b65ee8c9c486a22ad
SaladFrog <48181364+SaladFrog@users.noreply.github.com>
2020-12-27 22:02:37 +0100

    Update canvas-grid.js

--- canvas-grid.js
+++ canvas-grid.js
@@ -1,6 +1,3 @@
-/**
-* <canvas id='canvas'></canvas>
-*/
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 canvas.width = 400;
@@ -9,13 +6,11 @@ canvas.height = 400;
 const size = 20;
 const columns = Math.floor(canvas.width / size) + 1;
 const rows = Math.floor(canvas.height / size) + 1;
-
 const points = createGrid();
 
 points.forEach(([u, v]) => {
   const x = u * canvas.width;
   const y = v * canvas.height;
-
   ctx.beginPath();
   ctx.arc(x, y, 1, 0, Math.PI * 2);
   ctx.fillStyle = 'black';
