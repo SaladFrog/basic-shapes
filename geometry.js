4b5b40814bce68e1eb3a3d214dbea6865e568487
SaladFrog <48181364+SaladFrog@users.noreply.github.com>
2020-12-27 14:53:24 +0100

    Create geometry.js

---
+++ geometry.js
@@ -0,0 +1,16 @@
+// random dot on circle
+onCircle: function (out, radius) {
+	var theta = Math.random() * 2.0 * Math.PI;
+	out[0] = Math.cos(theta) * radius;
+	out[1] = Math.sin(theta) * radius;
+	return out;
+};
+
+// random dot in circle
+insideCircle: function (out, radius) {
+	var theta = Math.random() * 2.0 * Math.PI;
+	var r = radius * Math.sqrt(Math.random());
+	out[0] = Math.cos(theta) * r;
+	out[1] = Math.sin(theta) * r;
+	return out;
+};
