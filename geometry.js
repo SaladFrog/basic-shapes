0efb2b3410e4897f6f5f9f02d982a9887f39d60c
SaladFrog <48181364+SaladFrog@users.noreply.github.com>
2020-12-27 15:12:34 +0100

    Update geometry.js

--- geometry.js
+++ geometry.js
@@ -1,16 +1,22 @@
+/**
+* @param out: []
+* @param radius: int
+*
+* onCircle([], 100);
+*/
 // random dot on circle
 onCircle: function (out, radius) {
-	var theta = Math.random() * 2.0 * Math.PI;
-	out[0] = Math.cos(theta) * radius;
-	out[1] = Math.sin(theta) * radius;
+	var angle = Math.random() * 2.0 * Math.PI;
+	out[0] = Math.cos(angle) * radius;
+	out[1] = Math.sin(angle) * radius;
 	return out;
 };
 
 // random dot in circle
 insideCircle: function (out, radius) {
-	var theta = Math.random() * 2.0 * Math.PI;
+	var angle = Math.random() * 2.0 * Math.PI;
 	var r = radius * Math.sqrt(Math.random());
-	out[0] = Math.cos(theta) * r;
-	out[1] = Math.sin(theta) * r;
+	out[0] = Math.cos(angle) * r;
+	out[1] = Math.sin(angle) * r;
 	return out;
 }; 
