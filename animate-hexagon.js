3a114aebdcee562b8611c49cc00b7c6c7fa271fa
SaladFrog <48181364+SaladFrog@users.noreply.github.com>
2021-01-26 19:55:48 +0100

    Update animate-hexagon.js

--- animate-hexagon.js
+++ animate-hexagon.js
@@ -8,39 +8,44 @@ function lerp(start, end, amt) {
 }
 
 function draw(dt) {
-	console.log(dt)
   const dim = Math.min(width, height);
-  const date = new Date();
-  const time = date.getMilliseconds() / 1000;
+  context.strokeStyle = 'white';
+  const time = new Date().getTime() / 1000;;
 
   const rings = 10;
   const sides = 6;
   const maxRadius = dim * 0.4;
 
   for (let j = 0; j < rings; j++) {
-
     const t = (j + 1) / rings;
     const radius = t * maxRadius;
     const maxThickness = maxRadius / rings * 1;
-    const minThickness = maxRadius / rings * 0.001;
+    const minThickness = maxRadius / rings * 0.01;
     const pingPong = Math.sin(t * 3.0 + time) * 0.5 + 0.5;
-
     const thickness = lerp(minThickness, maxThickness, pingPong);
     context.lineWidth = thickness;
-
-    context.beginPath();
-    for (let i = 0; i < sides; i++) {
-      const a = Math.PI / 2 + Math.PI * 2.0 * (i / sides);
-      let sx = width / 2 + Math.cos(a) * radius;
-      let sy = height / 2 + Math.sin(a) * radius;
-      context.lineTo(sx, sy)
-      context.lineTo(sx, sy)
-    }
-    context.closePath()
-    context.stroke();
+    polygon(width/2, height/2, radius, sides, Math.PI /2);
   }
 }
-window.requestAnimationFrame(draw);
 
+function polygon(x, y, radius, sides = 3, angle = 0) {
+  context.beginPath();
+  for (let i = 0; i < sides; i++) {
+    const a = Math.PI / 2 + Math.PI * 2.0 * (i / sides);
+    let sx = x + Math.cos(a) * radius;
+    let sy = y + Math.sin(a) * radius;
+    context.lineTo(sx, sy)
+    context.lineTo(sx, sy)
+  }
+  context.closePath()
+  context.stroke();
+}
 
+function setup() {
+  context.fillStyle = 'black';
+  context.fillRect(0,0,width, height);
+  draw();
+  window.requestAnimationFrame(setup);
+}
 
+setup();
