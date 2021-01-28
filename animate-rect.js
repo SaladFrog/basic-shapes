e0c92d1f49b4dabaa0f1158ec988bf7536f07ed7
SaladFrog <48181364+SaladFrog@users.noreply.github.com>
2021-01-28 09:31:52 +0100

    Update animate-rect.js

--- animate-rect.js
+++ animate-rect.js
@@ -1,35 +1,38 @@
-// <canvas id="canvas"></canvas>
+const sketch = () => {
+  const canvas = document.getElementById('canvas');
+  const context = canvas.getContext('2d');
+  let width = canvas.width = 400;
+  let height = canvas.height = 400;
+  let angle = 0;
 
-const canvas = document.getElementById('canvas');
-const context = canvas.getContext('2d');
-let width = canvas.width = 400;
-let height = canvas.height = 400;
+  function start() {
+    clear();
+    draw();
+    window.requestAnimationFrame(start);
+  }
 
-let angle = 0;
+  function clear() {
+    context.fillStyle = 'black';
+    context.fillRect(0, 0, width, height);
+  }
 
-function setup() {
-  clear();
-  draw();
-  window.requestAnimationFrame(setup);
+  function draw(dt) {
+    angle += 1;
+    context.fillStyle = 'white';
+    context.save();
+    context.translate(width / 2, height / 2);
+    context.rotate(angle * Math.PI / 180);
+    context.fillRect(-20, -20, 40, 40)
+    context.restore();
+  }
+
+  function lerp(start, end, amt) {
+    return (1 - amt) * start + amt * end
+  }
+
+  return {
+    start: start,
+  }
 }
 
-function clear() {
-  context.fillStyle = 'black';
-  context.fillRect(0, 0, width, height);
-}
-
-function draw(dt) {
-  angle += 1;
-  context.fillStyle = 'white';
-  context.save();
-  context.translate(width/2, height/2);
-  context.rotate(angle *Math.PI/180);
-  context.fillRect(-20, -20, 40, 40)
-  context.restore();
-}
-
-function lerp(start, end, amt) {
-  return (1 - amt) * start + amt * end
-}
-
-setup();
+sketch().start();
