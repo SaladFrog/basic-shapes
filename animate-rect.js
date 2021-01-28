e999b74ce794527b96b8ddd2624e1afaa0a6bf2c
SaladFrog <48181364+SaladFrog@users.noreply.github.com>
2021-01-28 09:20:47 +0100

    Create animate-rect.js

---
+++ animate-rect.js
@@ -0,0 +1,35 @@
+// <canvas id="canvas"></canvas>
+
+const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
+let width = canvas.width = 400;
+let height = canvas.height = 400;
+
+let angle = 0;
+
+function setup() {
+  clear();
+  draw();
+  window.requestAnimationFrame(setup);
+}
+
+function clear() {
+  context.fillStyle = 'black';
+  context.fillRect(0, 0, width, height);
+}
+
+function draw(dt) {
+	angle += 1;
+  context.fillStyle = 'white';
+  context.save();
+  context.translate(width/2, height/2);
+  context.rotate(angle *Math.PI/180);
+  context.fillRect(-20, -20, 40, 40)
+  context.restore();
+}
+
+function lerp(start, end, amt) {
+  return (1 - amt) * start + amt * end
+}
+
+setup();
